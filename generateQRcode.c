69aff79f0ab18256029a3e82dafc2cb9051f3300
Damon Meng <pinecony.meng@gmail.com>
2018-03-10 16:09:10 -0500

    finished parted 1

--- generateQRcode.c
+++ generateQRcode.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
+
+
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 
 #include "lib/encoding.h"
+
+void addPadding(char * secret, int length) {
+	int i;
+	char padding[20] = "";
+	for (i = 0; i < length; i++) {
+		padding[i] = secret[i];
+	}
+	for (i = length; i < 20; i++) {
+		padding[i] = '0';
+	}
+	strcpy(secret, padding);
+}
+
+uint8_t charValue(char c) {
+	if(c >= '0' && c <= '9') {
+		return c - '0';
+	} else if(c >= 'a' && c <= 'f') {
+		return c - 'a' + 10;
+	} else if(c >= 'A' && c <= 'F') {
+		return c - 'A' + 10;
+	}
+	return 255;
+}
+
+void generateHex(char *secret, uint8_t * solution) {
+    uint8_t * c;
+    int i;
+    for(i=0, c = (uint8_t *) secret; i < 10; i++, c += 2) {
+        solution[i] = (charValue(*c) << 4) | charValue(*(c + 1));
+    }
+    solution[10] = 0;
+}
 
 int
 main(int argc, char * argv[])
@@ -19,13 +53,33 @@ main(int argc, char * argv[])
 
 	assert (strlen(secret_hex) <= 20);
 
-	printf("\nIssuer: %s\nAccount Name: %s\nSecret (Hex): %s\n\n",
+	char * encodedName = urlEncode(accountName);
+	char * encodedIssuer = urlEncode(issuer);
+
+	printf("\nIssuer: %s\nAccount Name: %s\nSecret (Hex): %s\n\n", 
 		issuer, accountName, secret_hex);
+
+	int secretLength = strlen(secret_hex);
+	if (secretLength < 20) {
+		addPadding(secret_hex, secretLength);
+	}
 
 	// Create an otpauth:// URI and display a QR code that's compatible
 	// with Google Authenticator
+	
+	generateHex(secret_hex, secret_hex);
+	char encodedSecret[20];
+	base32_encode(secret_hex, 10, encodedSecret, 20);
 
-	displayQRcode("otpauth://testing");
+	printf(encodedSecret);
+
+	char URI1[200];
+	sprintf(URI1, "otpauth://hotp/%s?issuer=%s&secret=%s&counter=1", encodedName, encodedIssuer, encodedSecret);
+	displayQRcode(URI1);
+
+	char URI2[200];
+	sprintf(URI2, "otpauth://totp/%s?issuer=%s&secret=%s&period=30", encodedName, encodedIssuer, encodedSecret);
+	displayQRcode(URI2);
 
 	return (0);
 }
